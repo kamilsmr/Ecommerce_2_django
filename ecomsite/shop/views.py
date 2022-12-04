e87640d06b075647ea862b88d8d423d8d27ae4ac
kamilsmr <kamils6035@gmail.com>
2022-12-05 00:14:53 +0300

    page

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -16,8 +16,15 @@ def index(request):
 
     #paginator code
     paginator = Paginator(product_objects,4)
-    page = request.Get.get('page')
-    product_objects = paginator.get_page(page)
+    # page = request.Get.get('page')
+    # product_objects = paginator.get_page(page)
     
     return render(request,'shop/index.html',{'product_objects' : product_objects})
+
+
+def detail(request,id):
+    product_object = Products.objects.get(id=id)
+    return render(request,'shop/detail.html',{'product_object':product_object})
+
+
     
